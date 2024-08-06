267b574e38805e045cc5264460b432fe3c8e79e9
CuongDepTrai <nguyenhuucuong111103@gmail.com>
2024-08-06 15:12:49 +0700

    first commit

---
+++ src/main/java/com/huucuong/TimeHub/TimeHubApplication.java
@@ -0,0 +1,13 @@
+package com.huucuong.TimeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huucuong/TimeHub/TimeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huucuong.TimeHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
