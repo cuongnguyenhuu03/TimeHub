c9319fbecd4b0940673b429998719ca8b34d73c4
CuongDepTrai <nguyenhuucuong111103@gmail.com>
2024-08-07 08:59:26 +0700

    add dependence

---
+++ src/main/java/com/huucuong/TimeHub/HelloController.java
@@ -0,0 +1,23 @@
+package com.huucuong.TimeHub;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from Spring Boot?!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "User";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Admin";
+    }
+}
