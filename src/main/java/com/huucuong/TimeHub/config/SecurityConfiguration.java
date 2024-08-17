d0bf09ad200b4be392cda09ef63992821d2e9b05
CuongDepTrai <nguyenhuucuong111103@gmail.com>
2024-08-17 17:03:27 +0700

    custom login page

--- src/main/java/com/huucuong/TimeHub/config/SecurityConfiguration.java
+++ src/main/java/com/huucuong/TimeHub/config/SecurityConfiguration.java
@@ -10,9 +10,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.huucuong.TimeHub.service.impl.CustomUserDetailsService;
 import com.huucuong.TimeHub.service.impl.UserService;
+
+import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -37,6 +40,21 @@ public class SecurityConfiguration {
         authProvider.setPasswordEncoder(passwordEncoder);
         // authProvider.setHideUserNotFoundExceptions(false);
         return authProvider;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE).permitAll()
+                        .requestMatchers("/login", "/client/**", "/images/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .failureUrl("/login?error")
+                        .permitAll());
+        return http.build();
     }
 
 }
